9f2ead9dbe6567bf5203016b4aa286f2ae2e3eca
SnIvanova <1snizhanaivanova@gmail.com>
2023-10-30 14:31:56 +0100

    practise and teoria of 1 day

---
+++ tg1/js/script.js
@@ -0,0 +1,53 @@
+//coment
+console.log('Hello World!');
+
+var news = 23;
+var news2 = 23;
+
+
+var strl = 'Hello World';
+
+var bol1 = true;
+var bol2 = false;
+
+var emp1 = null;
+var emp2 = undefined;
+
+var v;
+v = 'valore';
+var v2 = 'valore';
+
+var somma = news + news2;
+console.log(somma);
+console.log(strl + somma);
+
+var test = "test";
+ {
+    var innertest = "innertest";
+    console.log(innertest);
+ }
+ console.log(test);
+ console.log(innertest);
+
+ let data = "ciao";
+ console.log(typeof data);
+ data = 123;
+ console.log(typeof data);
+ data = true;
+ console.log(typeof data);
+
+ let n1 = 10;
+ n1 += 2
+console.log(n1);
+
+
+let n3 = 10;
+let n4 = "10";
+let n5 = 5;
+console.log(n3 !== n4 || n5);
+
+
+let eta = 10;
+let acesso = (eta >= 18 ) ? "acesso" : "non acesso";
+console.log(acesso);
+
