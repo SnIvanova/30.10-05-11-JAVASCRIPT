9c18e09e558231366828fdccd4518d24ed05f864
SnIvanova <1snizhanaivanova@gmail.com>
2023-10-31 15:10:41 +0100

    teoria e prattica g2

---
+++ tg2/script.js
@@ -0,0 +1,23 @@
+let nome = 'João';
+let cognome = 'Silva';
+let eta = 43;
+let citta = "roma";
+
+let utente = { nome, cognome, eta, citta };
+
+let utente1 = {
+    nome1: "milky",
+    cognome1: "way",
+    eta1: 43,
+    citta1: "roma"
+}
+
+console.log(utente1);
+console.log(utente['nome1']); //same as:
+console.log(utente.nome1);
+
+
+let arr = new Array ();
+let arr1 = [];
+arr.push ("colomba");
+console.log(arr);
