cc20b1809bf935cbf5f85321a2e3bdc4b2e7c8f1
SnIvanova <1snizhanaivanova@gmail.com>
2023-11-03 11:46:59 +0100

    Update D4.js

--- tg4/D4.js
+++ tg4/D4.js
@@ -4,13 +4,26 @@ function getRandomNumber(min, max) {
 
 function firstfunction() {
     console.log(getRandomNumber(1, 10));
-
 }
 
 function printString() {
-    console.log("hello world")
+    console.log("hello world");
 }
 
 firstfunction();
 
 printString();
+
+function somma() {
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+    }
+    return sum;
+}
+
+let smt = somma(1, 2, 3, 4, 5); // Pass arguments to the somma function
+console.log(smt);
+
+let smt2 = somma(smt, 10); // Pass the result of somma function and 10 as arguments
+console.log(smt2);
