4a6fba5253235104ef8aa0772431a22c3520fb19
SnIvanova <1snizhanaivanova@gmail.com>
2023-11-03 15:04:39 +0100

    practisse

--- tg4/D4.js
+++ tg4/D4.js
@@ -27,3 +27,25 @@ console.log(smt);
 
 let smt2 = somma(smt, 10); // Pass the result of somma function and 10 as arguments
 console.log(smt2);
+
+function func () {
+    console.log("hello" + arguments.length);
+    for (let i = 0; i < arguments.length; i++) {
+        console.log(arguments[i]);
+    }
+}
+
+func(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+
+var test = "test var";
+
+console.log(test);
+
+let funk2 = function () {
+    console.log("hello" + arguments.length);
+    for (let i = 0; i < arguments.length; i++) {
+        console.log(arguments[i]);
+    }
+}
+funk2 ();
